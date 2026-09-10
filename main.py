2a9ec5e384336425794d384ebf12727dbaa27378
oskar <oskaraszinecnko500@gmail.com>
2026-09-09 22:24:23 -0400

    Added wmi and psutil libraries to assist in the project started on figuring out some functions, first test getting CPU info and runs correctly

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+import wmi
+import psutil
+
+
+# Variable declaration 
+P_core_count = psutil.cpu_count(logical=False)
+L_core_count = psutil.cpu_count()
+
+print("--- CPU Information ---")
+print("Physical Core Count: " + str(P_core_count))
+print("Number of Threads: " + str(L_core_count))
+
