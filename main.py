c87cb4d83065c8971bb5a7ae5a3874a62016f951
oskar <oskaraszinecnko500@gmail.com>
2026-09-09 23:35:01 -0400

    fixed issue with missnumbered menu, also added the function display project info

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ running_script = True
 #Menu selection and redirection
 def direct_user(user_input):
     match user_input:
-        case "1":
+        case "0":
             display_project_info()
-        case "2":
+        case "1":
             display_general_hardware_info()
-        case "3":
+        case "2":
             display_cpu_info()
-        case "4":
+        case "3":
             display_gpu_info()
+        case "4":
+            display_ram_info()
         case "5":
             display_storage_info()
         case "6":
@@ -34,6 +36,20 @@ def direct_user(user_input):
 
 # Display info about the project and me
 def display_project_info():
+    print("\n\n" \
+    "\t\t -Project Information-\n" \
+    "Hello, welcome to my hardware monitor my name is Oskaras Zincenko\n" \
+    "As of writing this i am a student at CSU a member of the USCG and-\n" \
+    "I also currently work at best buy, i host a channel where i am going-\n" \
+    "to document my dev progress, i really enjoy learning about hardware.\n" \
+    "Although this V1 of the project is basic i plan to make a GUI out of-\n" \
+    "this and then want to possibly do something fancy for V3.\n" \
+    "it is currently 9/9/2026 my goals are\n" \
+    "gain hardware setup and experience\n" \
+    "cooler projects but like interesting ones not copy paste snooze fest\n" \
+    "get internship or co-op\n\n" \
+    "press enter to go back to menu!\n")
+    input()
     return
 
 # Display a general summary of the users hardware and specs
@@ -47,6 +63,10 @@ def display_cpu_info():
 # Display GPU info 
 def display_gpu_info():
     return
+
+# Display RAM info 
+def display_ram_info():
+    return 
 
 # Display Storage info
 def display_storage_info():
