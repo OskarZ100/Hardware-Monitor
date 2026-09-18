d54d3a9cb129d5da7e67da343e32a970973df9dd
OskarZ100 <oskaraszincenko500@gmail.com>
2026-09-17 23:32:33 -0400

    finally implemented free space counter for each individual letter drive and if its split into multiple partitions, this was one of the messier and harder things to write could be optimized and cleanier but will do that for V2

--- hardware.py
+++ hardware.py
@@ -82,11 +82,26 @@ class RAM(Hardware):
             count_for_ram += 1
         return final_string
 
+
+# LogicalDisk to partition object helper with the class below
+class LD2P_object:
+
+    def __init__(self,Device_id,Capacity,Freespace):
+        self.Device_id = Device_id
+        self.capacity = Capacity
+        self.freespace = Freespace
+
+
 class Storage:
+
+    def to_gb (self,num):
+        return ((int(num)) / (1000 ** 3))
 
     def __init__(self, hard_list, logic_list):
         self.physical_hardware = hard_list
         self.logical_hardware = logic_list
+        self.important_relations = self.important_setup()
+        self.valued_partition_count = self.valued_partition_ctSetup()
 
 
     def string_general_info(self):
@@ -100,13 +115,12 @@ class Storage:
             # This is the same with MediaType and InterfaceType but those can use a simple str wrap as we are not calculating anything
             try:
                 Capacity = str(round(((int(item.Size)) / (1000 ** 3)), 2))
-            except:
-                print("\n\n USERS DRIVE DOES NOT HAVE A DISPLAYABLE CAPACITY")
+            except(TypeError, ValueError):
                 Capacity = "NONE"
 
             final_string += "\t Drive "+ str(count) + " :" + item.Model + "\n"
             final_string += "\t\t Capacity: " + Capacity  + " GB\n"
-            final_string += "\t\t Free Space: " + self.free_space_return(count-1) + "\n"
+            final_string += self.free_space_return(count-1)
             final_string += "\t\t Media Type: " + str(item.MediaType) + "\n"
             final_string += "\t\t Interface: " + str(item.InterfaceType) + "\n\n"
             count += 1
@@ -117,8 +131,53 @@ class Storage:
         self.physical_hardware = hard_list
         self.logical_hardware = logic_list
 
-    def free_space_return(self,item_num):
-        #Use the disk partition WMI class for the object
-        #Using the disk index feature should allow for matching and finding the partitions associated with the physucal drive
-        #try this approach first 
-        return "placeholder"
+    def important_setup(self):
+        # Just found this extremly useful object, did not see it before gives so much good info and solves big partition problem
+        # Dependent is key here
+        relation = wmi.WMI().Win32_LogicalDiskToPartition()
+        
+        # Set up a dictionary for easy acess to important partitions
+        # Will map it out with ID:(this will be like OS and stuff so each logical disk gets matched to appropriate physical disk)
+        # Then each of these will containt a list of objects with info towards the free space per partiton
+        # This may make the editor a little packed but as this is first project with WMI i will optimize in V2
+        mapper = {}
+        for x in relation:
+            vol_name = str(x.Dependent.VolumeName)
+            temp_obj_holder = LD2P_object(str(x.Dependent.DeviceID),str(x.Dependent.Size),str(x.Dependent.FreeSpace))
+        
+            # Quick Key check for the dict
+            if mapper.get(vol_name) is None:
+                mapper[vol_name] = []
+        
+            # Add to the dict
+            mapper[vol_name].append(temp_obj_holder)
+
+        return mapper
+
+    def valued_partition_ctSetup(self):
+        arr_map = [0] * len(self.physical_hardware)
+        
+        setup_count = 0
+        for item in self.important_relations:
+            for part in item[1]:
+                arr_map[setup_count] += 1
+            setup_count += 1
+
+        return arr_map
+
+    def free_space_return(self,num):
+        final_string = ""
+
+        # This was the most messy function to write so far will clean up the whole process in V2
+        # Really happy i was able to get it running had me stumped definitly a little messy tried to clean it up where i could
+        for i in range(0, self.valued_partition_count[num]):
+            final_string += "\t    Drive Letter: " + str(list(self.important_relations.values())[num][i].Device_id) + "\n"
+            final_string += "\t\t Capacity of Drive: " +str(self.to_gb(int(list(self.important_relations.values())[num][i].capacity))) + " GB\n"
+            final_string += "\t\t Free space of Drive: " +str(self.to_gb(int(list(self.important_relations.values())[num][i].freespace))) + " GB\n\n"
+
+
+
+        return final_string
+
+
+
